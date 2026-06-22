1fe4589ec950576b877b527f254840f6e38b41b6
Nitesh Work <nitesh@mynextdeveloper.com>
2026-06-22 12:38:11 +0530

    Fix: Landing page section 2

--- src/components/ScrollIndicators.tsx
+++ src/components/ScrollIndicators.tsx
@@ -7,12 +7,12 @@ interface ScrollIndicatorsProps {
 
 export default function ScrollIndicators({ total, activeIndex, white, onScrollTo }: ScrollIndicatorsProps) {
   return (
-    <div className="fixed right-6 top-1/2 -translate-y-1/2 flex flex-col gap-3 z-50">
+    <div className="fixed right-5 top-1/2 -translate-y-1/2 flex flex-col gap-[7px] z-50">
       {Array.from({ length: total }).map((_, i) => (
         <button
           key={i}
           onClick={() => onScrollTo(i)}
-          className={`h-4 w-4 rounded-full transition-all duration-300 cursor-pointer border ${
+          className={`h-[10px] w-[10px] rounded-full transition-all duration-300 cursor-pointer border ${
             white
               ? `border-white ${i === activeIndex ? "bg-white" : "bg-transparent"}`
               : `border-black ${i === activeIndex ? "bg-black" : "bg-transparent"}`
